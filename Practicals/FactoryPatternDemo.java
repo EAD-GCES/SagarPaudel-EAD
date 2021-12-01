485a1f731a4df9c7ac31dc1e99c6503a82336568
Sagar Paudel <sagarpudel68@gmail.com>
2021-12-01 20:19:59 +0545

    factory pattern demo

---
+++ Practicals/FactoryPatternDemo.java
@@ -0,0 +1,50 @@
+interface Shape{
+    public void draw();
+}
+
+class Rectangle implements Shape{
+    public void draw(){
+        System.out.println("This is RECTANGLE");
+    }
+}
+
+class Square implements Shape{
+    public void draw(){
+        System.out.println("This is SQUARE");
+    }
+}
+
+class Circle implements Shape{
+    public void draw(){
+        System.out.println("This is CIRCLE");
+    }
+}
+
+class ObjectFactory {
+    public Shape getShape(String shapeName){
+        if(shapeName == null){
+            return null;
+        }
+        else if(shapeName.equalsIgnoreCase("CIRCLE")){
+            return new Circle();
+        }
+        else if(shapeName.equalsIgnoreCase("RECTANGLE")){
+            return new Rectangle();
+        }
+        else if(shapeName.equalsIgnoreCase("SQUARE")){
+            return new Square();
+        }
+        else{return null;}
+    }
+}
+
+public class FactoryPatternDemo{
+    public static void main(String args[]){
+        ObjectFactory objfactory = new ObjectFactory();
+        try{
+            Shape s=objfactory.getShape("");
+        }
+        
+        s.draw();
+    }
+}
